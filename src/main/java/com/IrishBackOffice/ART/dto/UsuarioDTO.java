e743ae44e5388aee95e14b524d31053c53b1bd2d
fernando <fernando.hasperue@gmail.com>
2025-03-01 16:53:21 -0300

    Se agrega configuracion de CORS

--- src/main/java/com/IrishBackOffice/ART/dto/UsuarioDTO.java
+++ src/main/java/com/IrishBackOffice/ART/dto/UsuarioDTO.java
@@ -11,6 +11,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 /**
  *
